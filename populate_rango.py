44712d993d72bd86718e93cc27d6619f4c1d849b
Tengyu Zhang <2519036z@student.gla.ac.uk>
2021-07-29 20:28:03 +0100

    chapter5 commit3

--- populate_rango.py
+++ populate_rango.py
@@ -17,9 +17,10 @@ def populate():
     cats = {'Python': {'pages': python_pages,'views':128,'likes': 64},'Django': {'pages': django_pages,'views':64,'likes': 32},'Other Frameworks': {'pages': other_pages,'views':32,'likes': 16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat,views=cat_data['views'],likes=cat_data['likes'])
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
+
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
@@ -32,11 +33,16 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name,views=0,likes=0):
+
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
+
+# Start execution here!
 if __name__ == '__main__':
     print('Starting Rango population script...')
     populate()
--- rango/models.py
+++ rango/models.py
@@ -14,5 +14,5 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
